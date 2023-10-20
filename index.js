c6c8043af99ccd660a3aef4da7cf35904e931118
Danyeis Bravo Ramirez <danyeistj@gmail.com>
2023-10-20 18:54:22 -0500

    port

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ startConnection();
 //   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 // });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+//const PORT = process.env.PORT || 5000;
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
