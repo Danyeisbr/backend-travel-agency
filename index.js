2b9ccb0fbaa8b8f559c90b5876064fde5b82df74
Danyeis Bravo Ramirez <danyeistj@gmail.com>
2023-10-20 19:25:20 -0500

    env

--- index.js
+++ index.js
@@ -1,29 +1,15 @@
-// import app from './app.js';
-// import { startConnection } from './database.js';
+import app from './app.js';
+import { startConnection } from './database.js';
 
-// startConnection();
-// app.listen(process.env.PORT || 4000, () => {
-//   console.log('Server is running on port: ' + process.env.PORT);
-//   if (!process.env.RESEND_API_KEY) {
-//     console.log(`Error: You need to define RESEND_API_KEY in the .env file.`);
-//   }
-//   console.log('Listening on http://localhost:4000');
-// });
+import { config } from "dotenv";
+config();
 
-// //This file is for azure deployment purposes
-const express = require('express');
-const path = require('path');
-const app = express();
-//import { startConnection } from './database.js';
 
-//startConnection();
-//app.use(express.static(path.join(__dirname, 'build')));
-
-// app.get('/*', function (req, res) {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
-
-//const PORT = process.env.PORT || 5000;
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
+startConnection();
+app.listen(process.env.PORT || 4000, () => {
+  console.log('Server is running on port: ' + process.env.PORT);
+  if (!process.env.RESEND_API_KEY) {
+    console.log(`Error: You need to define RESEND_API_KEY in the .env file.`);
+  }
+  console.log('Listening on http://localhost:4000');
 });
