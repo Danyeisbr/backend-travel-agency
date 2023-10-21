b7d1b2af4be7064eb27748e026ec04ef99be423f
Danyeis Bravo Ramirez <danyeistj@gmail.com>
2023-10-21 05:28:08 -0500

    changing cors

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import morgan from "morgan";
-import cors from "cors";
+
 import cookieParser from "cookie-parser";
 import hotelRoutes from "./routes/hotelRoutes.js";
 import roomRoutes from "./routes/roomRoutes.js";
@@ -9,18 +9,18 @@ import resendEmail from "./routes/resendEmail.js";
 import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
-app.use(cors());
+// app.use(cors());
 // const corsOptions = {
 //   origin: "https://front-travel-agency.azurewebsites.net",
 //   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
 //   credentials: true, // Enable cookies in the CORS requests
 //   optionsSuccessStatus: 204, // Some browsers return 204
 // };
-// app.use(function (request, response, next) {
-//   response.header("Access-Control-Allow-Origin", "*");
-//   response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+app.use(function (request, response, next) {
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(morgan("dev"));
 app.use(express.json());
