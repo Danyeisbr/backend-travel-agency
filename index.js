2918aa482de8470bcbff355814035d53bac79115
Danyeis Bravo Ramirez <danyeistj@gmail.com>
2023-10-21 13:53:22 -0500

    cors updated

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ import { startConnection } from './database.js';
 import { config } from "dotenv";
 config();
 
+app.get('/', function (req, res) {
+
+  res.header("Access-Control-Allow-Origin", "*");
+  res.send('hello world')
+})
+
 startConnection();
 // app.listen(process.env.PORT || 4000, () => {
 //   console.log('Server is running on port: ' + process.env.PORT);
