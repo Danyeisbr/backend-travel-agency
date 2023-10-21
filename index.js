89fd6c6e0cffd56e4b5171224f41c62f1856e600
Danyeis Bravo Ramirez <danyeistj@gmail.com>
2023-10-20 19:40:59 -0500

    port

--- index.js
+++ index.js
@@ -6,10 +6,14 @@ config();
 
 
 startConnection();
-app.listen(process.env.PORT || 4000, () => {
-  console.log('Server is running on port: ' + process.env.PORT);
-  if (!process.env.RESEND_API_KEY) {
-    console.log(`Error: You need to define RESEND_API_KEY in the .env file.`);
-  }
-  console.log('Listening on http://localhost:4000');
+// app.listen(process.env.PORT || 4000, () => {
+//   console.log('Server is running on port: ' + process.env.PORT);
+//   if (!process.env.RESEND_API_KEY) {
+//     console.log(`Error: You need to define RESEND_API_KEY in the .env file.`);
+//   }
+//   console.log('Listening on http://localhost:4000');
+// });
+
+app.listen(8080, '0.0.0.0', () => {
+  console.log('Server running at http://0.0.0.0:8080/');
 });
